a11ed91567dd65095ab5f82cf173226580d11cf2
jod35 <jodestrevin@gmail.com>
2020-08-06 15:29:24 -0700

    add(login and signup templates)

--- house_app/urls.py
+++ house_app/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name='appl'
 
 urlpatterns = [
-    path('',views.index,name='home')
+    path('',views.index,name='home'),
+    path('signup/',views.create_account,name='register'),
+    path('login/',views.login,name='login'),
 ]
--- house_app/views.py
+++ house_app/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
+from .forms import RegistrationForm
 
 # Create your views here.
 
 def index(request):
     return render(request,'house_app/index.html')
+
+
+def create_account(request):
+    form=RegistrationForm()
+    context={
+        'form':form
+    }
+    return render(request,'house_app/signup.html',context)
+
+
+def login(request):
+    return render(request,'house_app/login.html')
