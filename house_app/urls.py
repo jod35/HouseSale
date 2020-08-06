64adf85dc72ef50f66a01881e794e358c505106e
jod35 <jodestrevin@gmail.com>
2020-08-06 15:49:10 -0700

    add(login view)

--- house_app/urls.py
+++ house_app/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 app_name='appl'
 
 urlpatterns = [
     path('',views.index,name='home'),
     path('signup/',views.create_account,name='register'),
-    path('login/',views.login,name='login'),
+    path('login/',auth_views.LoginView.as_view(template_name='house_app/login.html'),name='login'),
 ]
