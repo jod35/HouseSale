30da054609642a8900c9083972167f5398d1c189
jod35 <jodestrevin@gmail.com>
2020-08-05 19:42:02 -0700

    initial commit

---
+++ js/index.js
@@ -0,0 +1,27 @@
+const menu_open_button=document.querySelector('.menu-open-btn');
+const menu_close_button=document.querySelector('.menu-close-btn');
+const mobile_menu=document.querySelector('.mobile-nav-items');
+const mobile_links=document.querySelector('.nav-items');
+
+menu_open_button.addEventListener('click',displayMobileNav);
+
+
+function displayMobileNav(){
+    menu_open_button.style.display="none";
+    // mobile_menu.style.display="flex";
+    mobile_links.style.display="block";
+    mobile_menu.style.height="100vh";
+    
+    menu_close_button.style.display="block";
+}
+
+menu_close_button.addEventListener('click',closeMobileNav);
+
+
+function closeMobileNav(){
+    menu_close_button.style.display="none";
+    // mobile_menu.style.display="none";
+    mobile_links.style.display="none";
+    mobile_menu.style.height="0vh";
+    menu_open_button.style.display="block";
+}
