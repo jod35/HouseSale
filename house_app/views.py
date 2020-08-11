c978060f8b79f69ae6d6393211d8f724d47a81cb
jod35 <jodestrevin@gmail.com>
2020-08-10 23:44:38 -0700

    feat(warehouse update view)

--- house_app/views.py
+++ house_app/views.py
@@ -17,7 +17,9 @@ def create_account(request):
     if request.method == 'POST':
         form=RegistrationForm(request.POST)
         if form.is_valid():
+
             form.save()
+
             messages.success(request,'Account Created Successfully, You can login.')
 
             return redirect('/login')
@@ -31,14 +33,17 @@ def create_account(request):
 
 #dashboard_view
 def user_dashboard(request):
-    houses=House.objects.order_by('-id').all()
-    warehouses=WareHouse.objects.order_by('-id').all()
-    land=Land.objects.order_by('-id').all
-    context={
-        'houses':houses,
-        'warehouses':warehouses,
-        'land':land,
-        'title':'User Dashboard'
+    houses = House.objects.order_by('-id').all()
+
+    warehouses = WareHouse.objects.order_by('-id').all()
+
+    land = Land.objects.order_by('-id').all
+
+    context = {
+        'houses' : houses,
+        'warehouses' : warehouses,
+        'land' : land,
+        'title' : 'User Dashboard'
     }
     return render(request,'house_app/dashboard.html',context)
 
@@ -68,30 +73,41 @@ def create_house(request):
 
 #view for showing houses
 def house_admin(request):
-    houses=House.objects.filter(dealer=request.user).all()
-    context={
-        'title':"House Admin",
-        'houses':houses
+    houses = House.objects.filter(dealer=request.user).all()
+
+    context = {
+        'title' : "House Admin",
+        'houses' : houses
     }
     return render(request,'house_app/houses.html',context)
 
 class HouseUpdateView(UpdateView):
-    model=House
-    fields=['name','location','price','bathrooms','bedrooms','toilets','sitting_room','swimming_pool','image1']
-    template_name='house_app/updatehouse.html'
-    success_url='/view_houses'
-    context_object_name='house'
+    model = House
+
+    fields=  ['name','location','price','bathrooms','bedrooms','toilets','sitting_room','swimming_pool','image1']
+    
+    template_name = 'house_app/updatehouse.html'
+    
+    success_url = '/view_houses'
+    
+    context_object_name = 'house'
 
 def delete_house(request,id):
-    house=House.objects.get(id=id)
+
+    house = House.objects.get(id=id)
 
     if request.method == 'POST':
+
         house.delete()
+        
         messages.success(request,"House Deleted Successfully")
+        
         return redirect('/view_houses/')
-    context={
+    
+    context = {
         'house':house
     }
+    
     return render(request,'house_app/deletehouse.html',context)
 
 #view for creating a warehouse
@@ -122,16 +138,30 @@ def create_warehouse(request):
 #list view for warehouses
 def view_warehouses(request):
     warehouses = WareHouse.objects.filter(dealer=request.user).all()
+
     context={
         "title" : "Warehouse Admin",
         "warehouses" : warehouses
     }
+
     return render(request,'house_app/warehouses.html',context)
+
+#update a warehouse 
+
+class WareHouseUpdateView(UpdateView):
+    model = WareHouse
+
+    fields = ['name','location','price','space','image1']
+
+    success_url = '/view_warehouses/'
+
+    template_name = 'house_app/updatewarehouse.html'
 
 
 #view for creating a land deal
 def create_land(request):
-    form=LandCreationForm()
+    form = LandCreationForm()
+
     context = {
         'form':form
     }
